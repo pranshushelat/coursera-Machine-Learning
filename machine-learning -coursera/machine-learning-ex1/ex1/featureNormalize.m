6a0a29c5aeb1ba8963423bca98e4e469f9e7c494
pranshushelat <shelatp@gmail.com>
2017-11-04 12:16:11 -0700

    Week 2 Assignment
    Assignment 1 files

---
+++ machine-learning -coursera/machine-learning-ex1/ex1/featureNormalize.m
@@ -0,0 +1,21 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% You need to set these values correctly
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+
+% The mean and standard deviation
+mu = mean(X);
+sigma = std(X);
+
+% Normalizing the features
+X_norm = X_norm .- mu;
+X_norm = X_norm ./ sigma;
+
+end
